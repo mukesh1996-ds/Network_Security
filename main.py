42bcf9fc12c060dffee0543c33853015e0f72315
mukesh1996-ds <mks.mukesh1996@gmail.com>
2025-06-20 08:36:57 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation, DataValidationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
@@ -13,6 +14,17 @@ if __name__=="__main__":
         data_ingstion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the Data Ingestion")
         dataingestionartifacts = data_ingstion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(dataingestionartifacts)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifacts,data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
+        
+
+        
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- test.py
+++
@@ -1,4 +0,0 @@
-from bson import SON
-from collections.abc import MutableMapping
-
-print("All good!")
