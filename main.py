b58a406ead9110e2df2d50ce593d4a5ef6431939
mukesh1996-ds <mks.mukesh1996@gmail.com>
2025-06-21 10:03:29 +0530

    Data Validation State Completed

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_transfromation import DataTransformation
+from networksecurity.entity.config_entity import DataTransformationConfig
 import os,sys
 
 
@@ -15,6 +17,7 @@ if __name__=="__main__":
         logging.info("Initiate the Data Ingestion")
         dataingestionartifacts = data_ingstion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
+
         print(dataingestionartifacts)
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(dataingestionartifacts,data_validation_config)
@@ -22,9 +25,13 @@ if __name__=="__main__":
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_validation_artifact)
-        
 
-        
+        logging.info("Data Tranformation Started")
+        data_transfromation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transfromation_config)
+        data_transformation_artifacts = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifacts)
+        logging.info("Data Transformation Completed")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -32,6 +32,7 @@ class DataIngestionConfig:
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
 
+
 class DataValidationConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir,training_pipeline.DATA_VALIDATION_DIR_NAME)
@@ -45,4 +46,19 @@ class DataValidationConfig:
                                                         training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
                                                         training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
                                                         )
+        
+class DataTransformationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,
+                                                         training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
+        self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,
+                                                            training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,
+                                                            training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
+        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, 
+                                                            training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_PATH,
+            training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
+        
         
